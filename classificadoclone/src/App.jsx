f60266327c745e3f153dda8852b6e9d7b9b84feb
Mount-Blanc <mountblanc100@gmail.com>
2023-06-14 12:42:36 -0700

    added basic elements of header component

--- classificadoclone/src/App.jsx
+++ classificadoclone/src/App.jsx
@@ -5,7 +5,21 @@ function App() {
 
   return (
     <>
-     
+     <header>
+      <div>
+        <button>nav</button>
+      <p>Logo</p>
+<button>Cambiar Area</button>
+      </div>
+      <div>
+        <span>
+          search
+        </span>
+        <a href=''>Mis Anuncios</a>
+        <a href=''>Publicar anuncio GRATIS</a>
+
+      </div>
+      </header>
     </>
   )
 }
