180b51811c3824037911fbda9c4bccbb437626dd
Mount-Blanc <mountblanc100@gmail.com>
2023-06-16 10:23:28 -0700

    added new section

--- classificadoclone/src/App.jsx
+++ classificadoclone/src/App.jsx
@@ -5,83 +5,83 @@ function App() {
 
   return (
     <>
-     <header>
-      <div>
-        <button>nav</button>
-      <p>Logo</p>
-<button>Cambiar Area</button>
-      </div>
-      <div>
-        <span>
-          search
-        </span>
-        <a href=''>Mis Anuncios</a>
-        <a href=''>Publicar anuncio GRATIS</a>
+      <header>
+        <div>
+          <button>nav</button>
+          <p>Logo</p>
+          <button>Cambiar Area</button>
+        </div>
+        <div>
+          <span>
+            search
+          </span>
+          <a href=''>Mis Anuncios</a>
+          <a href=''>Publicar anuncio GRATIS</a>
 
-      </div>
+        </div>
       </header>
       <div className='jumbo-tron'>
-      <div className="top-location-container">
-        <div>
-          <p>insert image</p>
+        <div className="top-location-container">
+          <div>
+            <p>insert image</p>
+          </div>
+          <div className='categories-container'>
+            <div>
+              <h4>Ciudades en California</h4>
+              <ul>
+                <li><a href=''>Los Angeles</a></li>
+                <li><a href=''>Orange County</a></li>
+                <li><a href=''>San Bernardino</a></li>
+                <li><a href=''>Riverside</a></li>
+                <li><a href=''>San Diego</a></li>
+                <li><a href=''>Bakersfield</a></li>
+                <li><a href=''>Fresno</a></li>
+                <li><a href=''>mas</a></li>
+
+              </ul>
+            </div>
+            <div>
+              <h4>Latinoamerica</h4>
+              <ul>
+                <li><a href=''>Bogota</a></li>
+                <li><a href=''>Lima</a></li>
+                <li><a href=''>Mexico DF</a></li>
+                <li><a href=''>Quito</a></li>
+                <li><a href=''>Guayaquil</a></li>
+                <li><a href=''>uatemala City</a></li>
+                <li><a href=''>Santiago</a></li>
+                <li><a href=''>Buenos Aires</a></li>
+
+              </ul>
+              <ul>
+                <li><a href=''>Caracas</a></li>
+                <li><a href=''>San Juan</a></li>
+                <li><a href=''>Medelin</a></li>
+                <li><a href=''>Cali</a></li>
+                <li><a href=''>Queretaro</a></li>
+                <li><a href=''>Ciudad Panama</a></li>
+                <li><a href=''>mas</a></li>
+
+              </ul>
+            </div>
+            <div>
+              <h4>Ciudades en E.E.U.U</h4>
+              <ul>
+                <li><a href=''>Los Angeles</a></li>
+                <li><a href=''>Orange County</a></li>
+                <li><a href=''>San Bernardino</a></li>
+                <li><a href=''>Riverside</a></li>
+                <li><a href=''>San Diego</a></li>
+                <li><a href=''>Bakersfield</a></li>
+                <li><a href=''>Fresno</a></li>
+                <li><a href=''>mas</a></li>
+
+              </ul>
+            </div>
+
+
+          </div>
         </div>
-        <div className='categories-container'>
-          <div>
-            <h4>Ciudades en California</h4>
-            <ul>
-              <li><a href=''>Los Angeles</a></li>
-              <li><a href=''>Orange County</a></li>
-              <li><a href=''>San Bernardino</a></li>
-              <li><a href=''>Riverside</a></li>
-              <li><a href=''>San Diego</a></li>
-              <li><a href=''>Bakersfield</a></li>
-              <li><a href=''>Fresno</a></li>
-              <li><a href=''>mas</a></li>
-
-            </ul>
-          </div>
-          <div>
-            <h4>Latinoamerica</h4>
-            <ul>
-              <li><a href=''>Bogota</a></li>
-              <li><a href=''>Lima</a></li>
-              <li><a href=''>Mexico DF</a></li>
-              <li><a href=''>Quito</a></li>
-              <li><a href=''>Guayaquil</a></li>
-              <li><a href=''>uatemala City</a></li>
-              <li><a href=''>Santiago</a></li>
-              <li><a href=''>Buenos Aires</a></li>
-
-            </ul>
-            <ul>
-              <li><a href=''>Caracas</a></li>
-              <li><a href=''>San Juan</a></li>
-              <li><a href=''>Medelin</a></li>
-              <li><a href=''>Cali</a></li>
-              <li><a href=''>Queretaro</a></li>
-              <li><a href=''>Ciudad Panama</a></li>
-              <li><a href=''>mas</a></li>
-
-            </ul>
-          </div>
-          <div>
-            <h4>Ciudades en E.E.U.U</h4>
-            <ul>
-              <li><a href=''>Los Angeles</a></li>
-              <li><a href=''>Orange County</a></li>
-              <li><a href=''>San Bernardino</a></li>
-              <li><a href=''>Riverside</a></li>
-              <li><a href=''>San Diego</a></li>
-              <li><a href=''>Bakersfield</a></li>
-              <li><a href=''>Fresno</a></li>
-              <li><a href=''>mas</a></li>
-
-            </ul>
-          </div>
-         
-
-        </div>
-      </div>
       </div>
       <div>
         <h2>Anuncios y Clasificados GRATIS en x</h2>
@@ -99,6 +99,19 @@ function App() {
           </ul>
         </div>
       </div>
+
+      <div className='Announcements'>
+        <div>
+        <h2>Anuncios destacados</h2>
+        <div>
+          <h3>THis is an example</h3>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      </div>
+
       <footer>
         <div>
           <div>
